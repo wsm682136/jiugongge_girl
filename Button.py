a7ae10f030544b3a4c76ac6cff28ea7a4582a320
liuxs <937587312@qq.com>
2023-10-27 11:45:52 +0800

    添加button类

---
+++ Button.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+
+
+import pygame
+
+
+class Button:
+
+    def __init__(self, x, y, w, h, txt, bc, font):
+        self.rect = pygame.Rect(x, y, w, h)
+        self.txt = txt
+        self.bc = bc
+        self.font = font
+        self.clicked = False
+
+    def draw(self, surface):
+        if self.clicked:
+            pygame.draw.rect(surface, (255, 0, 0), self.rect)
+        else:
+            pygame.draw.rect(surface, self.bc, self.rect)
+        pygame.draw.rect(surface, self.bc, self.rect)
+        font = pygame.font.Font(self.font, 26)
+        text_surface = font.render(self.txt, True, (255, 255, 255))
+        text_rect = text_surface.get_rect(center=self.rect.center)
+        surface.blit(text_surface, text_rect)
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(event.pos):
+                self.clicked = True
+        elif event.type == pygame.MOUSEBUTTONUP:
+            self.clicked = False
