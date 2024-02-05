2d39ee8ff2090f83f23cec96d75cad5553955de2
liuxs <937587312@qq.com>
2024-02-05 13:31:53 +0800

    '添加按钮字体大小'

--- Button.py
+++ Button.py
@@ -6,11 +6,12 @@ import pygame
 
 class Button:
 
-    def __init__(self, x, y, w, h, txt, bc, font):
+    def __init__(self, x, y, w, h, txt, bc, font, fontSize):
         self.rect = pygame.Rect(x, y, w, h)
         self.txt = txt
         self.bc = bc
         self.font = font
+        self.fontSize = fontSize
         self.clicked = False
 
     def draw(self, surface):
@@ -19,7 +20,7 @@ class Button:
         else:
             pygame.draw.rect(surface, self.bc, self.rect)
         pygame.draw.rect(surface, self.bc, self.rect)
-        font = pygame.font.Font(self.font, 26)
+        font = pygame.font.Font(self.font, self.fontSize)
         text_surface = font.render(self.txt, True, (255, 255, 255))
         text_rect = text_surface.get_rect(center=self.rect.center)
         surface.blit(text_surface, text_rect)
